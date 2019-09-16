698a079b1f974035cf3dd0da392bf07bd10c6336
Marcos Gomez <marcosgomezsanchez@gmail.com>
2019-09-16 18:58:34 +0100

    Admin-user control panel

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -2,9 +2,20 @@
 
 const { Router } = require('express');
 const router = Router();
+const User = require('./../../models/user');
 
-router.get('/', (req, res, next) => {
-  // res.render('index', { title: 'Hello World!' });
+router.get('/users', (req, res, next) => {
+  
+  User.find({})
+  .then(users => res.render('admin/users', {users} ))
+  .catch(err => console.log(err));
+});
+
+router.post('/users/delete', (req, res, next) => {
+  
+  User.delete({})
+  .then(users => res.render('admin/users', {users} ))
+  .catch(err => console.log(err));
 });
 
 module.exports = router;
