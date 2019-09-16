1bd17bac01ae5c89e6e021d7e4cc491381aefd12
Miguel Carrilho <mangelott@gmail.com>
2019-09-16 13:41:12 +0100

    shop view and route almost done. before lunch

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,43 @@
 'use strict';
+
 const { Router } = require('express');
 const router = Router();
+const path    = require('path');
+
+router.set('view engine', 'hbs');
+router.set('views', path.join(__dirname, 'views'));
+
+router.registerPartials(__dirname + '/views/partials');
+
+router.use(Router.static(path.join(__dirname, 'public')));
+
+const shops=[
+  {
+    'name': 'Prozis',
+    'location': {address:'Rua A', city:'Porto'},
+    'type': 'Sports Nutrition',
+    'contact': {email: 'geral@prozis.pt', phone:'00351', website:'www.prozis.pt'},
+    'photo': 'https://varzim.pt/site/wp-content/uploads/2016/07/prozis.jpg'
+  },
+  {
+    'name': 'Celeiro',
+    'location': {address:'Rua B', city:'Lisboa'},
+    'type': 'Health/Nature Shop',
+    'contact': {email: 'geral@celeiro.pt', phone:'00352', website:'www.celeiro.pt'},
+    'photo': 'https://www.celeiro.pt/pub/media/contentmanager/content/cache/400x400/sobre-o-celeiro/logo-celeiro.jpg'
+  }
+];
 
 router.get('/shops', (req, res, next) => {
-  res.render('');
+  res.render('shop', {shops});
 });
+
 router.post('/shops', (req, res, next) => {
   res.render('');
 });
-router.get('/shops:id', (req, res, next) => {
-  res.render('');
+
+router.get('/shops/:shopsId', (req, res, next) => {
+  res.send(req.params);
 });
 
 module.exports = router;
