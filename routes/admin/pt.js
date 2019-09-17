a7b1d50421fd5cc867e9d200bf05b03da7cbbbee
Marcos Gomez <marcosgomezsanchez@gmail.com>
2019-09-17 11:45:47 +0100

    Personal Trainer admin control panel

--- routes/admin/pt.js
+++ routes/admin/pt.js
@@ -2,9 +2,30 @@
 
 const { Router } = require('express');
 const router = Router();
+const User = require('./../../models/user');
 
-router.get('/', (req, res, next) => {
-  // res.render('index', { title: 'Hello World!' });
+router.get('/pt', (req, res, next) => {
+  User.find({role: 'personalTrainer'})
+  .then(pts => res.render('admin/pt', {pts} ))
+  .catch(err => console.log(err));
+});
+
+router.get('/pt/list', (req, res, next) => {
+  User.find({
+    role: 'personalTrainer',
+    name: {$regex : `${ req.query.ptName }`}
+})
+  .then(pts => res.render('admin/pt', {pts} ))
+  .catch(err => console.log(err));
+});
+
+router.post('/pt/delete/:ptId', (req, res, next) => {
+  User.findByIdAndDelete(req.params.ptId)
+  .then(pt => {
+    console.log( `${pt} was deleted.`);
+    res.redirect('/');
+})
+  .catch(err => console.log(err));
 });
 
 module.exports = router;
