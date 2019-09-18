9a47be53093b4166c37abae836160494b556d378
Marcos Gomez <marcosgomezsanchez@gmail.com>
2019-09-18 17:42:05 +0100

    form styles

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -5,7 +5,7 @@ const router = Router();
 const User = require('./../../models/user');
 
 router.get('/users', (req, res, next) => {
-  User.find({role: 'client'})
+  User.find({})
   .then(users => res.render('admin/users', {users} ))
   .catch(err => console.log(err));
 });
